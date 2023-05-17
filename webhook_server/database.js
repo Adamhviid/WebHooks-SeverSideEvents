9d1c8161de8d36c82801a66bbf8f9d0482766c4d
Adam <hviidadam@gmail.com>
2023-05-17 14:56:58 +0200

    cleanup

--- webhook_server/database.js
+++ webhook_server/database.js
@@ -1,8 +1,6 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
-const db = new sqlite3.Database('./webhooks.db')
-
 async function getSubscribers() {
   let users = [];
   const db = await openDb();
